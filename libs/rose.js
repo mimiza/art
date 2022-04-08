2bd56ea90f2f3a147e0a97f527bccdecae9207aa
dev@mimiza.com <dev@mimiza.com>
2022-04-08 22:52:11 +0700

    next

--- libs/rose.js
+++ libs/rose.js
@@ -7,16 +7,16 @@ function setup() {
 
 function draw() {
     translate(width/2, height/2)
-    const n = random(1, 7)
-    const d = random(1, 9)
+    const n = 2
+    const d = 5
     const k = n/d
-    clear()
+    stroke('green')
+    strokeWeight(3)
     beginShape()
-    stroke(color(random(0,255), random(0,255), random(0,255)))
-    for (a = 0; a < TWO_PI * 10; a += 0.1) {
-        let r = sin(k * a * noise(1,20) * 100) * width * 0.45
-        let x = r * cos(a)
-        let y = r * sin(a)
+    for (a = 0; a <= TWO_PI * d; a += 0.01) {
+        const r = cos(k * a) * width * 0.45
+        const x = r * cos(a)
+        const y = r * sin(a)
         vertex(x, y)
     }
     endShape()
